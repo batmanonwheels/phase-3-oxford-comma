be895a7ce955dc7267fd5510807963701ce10d61
Devin Garcia <devingarcia5423@gmail.com>
2022-06-29 21:07:03 -0400

    commit tree 29d5d29210fa2eb83d122d3bd1d761d5703d1e67

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  if array.length() > 2
+    last_word = array.pop
+    new_string = array.join(", ")
+    new_string << ", and #{last_word}"
+  elsif array.length() == 2
+    array.join(" and ")
+  else
+    array.join
+  end
 end
+
+oxford_comma(["fiddleheads", "okra", "kohlrabi"])
